9ecf94b1b1cc10b2b8c8948444838a1750d88ece
shahzaib-1-no <sa4715228@gmail,com>
2025-10-06 16:20:50 +0500

    roles and permission front code

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,58 +4,72 @@ from django.contrib.auth import authenticate, login as user_login, logout as use
 from django.contrib import messages
 from dashboard.forms import LoginForm, GroupForm
 
+
 @login_required
 def dashboard(request):
-    return render(request,'dashboard/dashboard.html')
+    return render(request, "dashboard/dashboard.html")
+
 
 def login(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = LoginForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            remember = form.cleaned_data.get('remember')
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            remember = form.cleaned_data.get("remember")
             user = authenticate(username=username, password=password)
             if user is not None:
                 user_login(request, user)
                 # remember me logic: session expiry set karein
                 if not remember:
                     request.session.set_expiry(0)  # browser close hone pe logout
-                return redirect('dashboard')
+                return redirect("dashboard")
             else:
-                messages.error(request,'Invalid username or password')
-                #form.add_error(None, "Invalid username or password")
+                messages.error(request, "Invalid username or password")
+                # form.add_error(None, "Invalid username or password")
         else:
-              print("Form is not valid:", form.errors)
+            print("Form is not valid:", form.errors)
     else:
         form = LoginForm()
-    context = {
-        'form': form
-    }
-    return render(request,'dashboard/auth/login.html',context)
+    context = {"form": form}
+    return render(request, "dashboard/auth/login.html", context)
+
 
 def logout(request):
     user_logout(request)
-    messages.success(request,'You have been logged out')
-    return redirect('login')
+    messages.success(request, "You have been logged out")
+    return redirect("login")
+
 
 @login_required
 def add_group(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = GroupForm(request.POST)
         if form.is_valid():
             group = form.save(commit=False)
             group.save()
-            messages.success(request,'Group added successfully')
-            return redirect('dashboard')
+            messages.success(request, "Group added successfully")
+            return redirect("dashboard")
         else:
-            messages.error(request,'Group not added')
+            messages.error(request, "Group not added")
     else:
         form = GroupForm()
-    context = {
-        'form': form
-    }
-    return render(request,'dashboard/add_group.html',context)
+    context = {"form": form}
+    return render(request, "dashboard/add_group.html", context)
+
+
 @login_required
 def user_role_and_permission(request):
-    return render(request,'dashboard/roles_and_permission.html')
+    if request.method == "POST":
+        form = GroupForm(request.POST)
+        if form.is_valid():
+            group = form.save(commit=False)
+            group.save()
+            messages.success(request, "Group added successfully")
+            return redirect("dashboard")
+        else:
+            messages.error(request, "Group not added")
+    else:
+        form = GroupForm()
+    context = {"form": form}
+    return render(request, "dashboard/roles_and_permission.html", context)
